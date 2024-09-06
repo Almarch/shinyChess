6043b7280c95b84ff15faef7c9187357e6cc7820
Almarch <alexandre.marchal@hotmail.com>
2024-09-06 22:41:54 +0200

    add everything for posit (no stockfish build step)

--- app/app.R
+++ app/app.R
@@ -3,7 +3,7 @@ install.packages('https://cran.r-project.org/src/contrib/Archive/rchess/rchess_0
 install.packages('https://cran.r-project.org/src/contrib/Archive/stockfish/stockfish_1.0.0.tar.gz', repos = NULL, type = 'source')
 install.packages('bigchess', version = '1.9.1')
 
-devtools::install_github("https://github.com/Almarch/shinyChess")
+devtools::install_github("https://github.com/Almarch/shinyChess/tree/posit")
 
 library(shinyChess)
 
