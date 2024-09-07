1986b0b63532ab249ce3ff693825af2db2af3d60
Almarch <alexandre.marchal@hotmail.com>
2024-09-07 16:41:00 +0200

    remove the renv things, but keep the def stockfish binary: works

--- app/app.R
+++ app/app.R
@@ -1,16 +1,7 @@
 
-library(renv)
-
-if(F){
-  renv::init()
-  devtools::install_github("https://github.com/curso-r/stockfish",force=T)
-  devtools::install_github("https://github.com/jbkunst/rchess",force=T)
-  devtools::install_github("https://github.com/almarch/shinyChess/tree/posit",force=T)
-  renv::snapshot()
-} else {
-  renv::activate()
-}
-
+devtools::install_github("https://github.com/curso-r/stockfish",force=T)
+devtools::install_github("https://github.com/jbkunst/rchess",force=T)
+devtools::install_github("https://github.com/almarch/shinyChess/tree/posit",force=T)
 library(shinyChess)
 
 app()
